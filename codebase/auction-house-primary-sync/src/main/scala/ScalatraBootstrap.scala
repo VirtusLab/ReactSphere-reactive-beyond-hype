333ce58bbd4fe9bd4001bc795e5a148c62e37ed9
mzagorski <zagorski.dev@gmail.com>
2018-03-02 14:59:58 +0100

    added get _status

--- codebase/auction-house-primary-sync/src/main/scala/ScalatraBootstrap.scala
+++ codebase/auction-house-primary-sync/src/main/scala/ScalatraBootstrap.scala
@@ -1,16 +1,19 @@
 
+import com.virtuslab.auctionHouse.sync.StatusServlet
 import javax.servlet.ServletContext
-
 import com.virtuslab.auctionHouse.sync.accounts.AccountsServlet
 import com.virtuslab.auctionHouse.sync.auctions.AuctionsServlet
 import com.virtuslab.auctionHouse.sync.signIn.SignInServlet
 import org.scalatra._
 
 class ScalatraBootstrap extends LifeCycle {
+  val apiVersion = "/api/v1"
+
   override def init(context: ServletContext) {
-    context.mount(new AccountsServlet, "/accounts/*")
-    context.mount(new SignInServlet, "/sign-in/*")
-    context.mount(new AuctionsServlet, "/auctions/*")
+    context.mount(new StatusServlet, "/*")
+    context.mount(new AccountsServlet, s"$apiVersion/accounts/*")
+    context.mount(new SignInServlet, s"$apiVersion/sign-in/*")
+    context.mount(new AuctionsServlet, s"$apiVersion/auctions/*")
   }
 }
 
