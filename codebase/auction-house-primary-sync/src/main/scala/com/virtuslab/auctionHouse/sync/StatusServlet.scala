fd5e24c07861c311aa12e4e67bd9a3054a9959aa
zagorski <zagorski.dev@gmail.com>
2018-03-02 16:47:36 +0100

    added get _status (#5)

---
+++ codebase/auction-house-primary-sync/src/main/scala/com/virtuslab/auctionHouse/sync/StatusServlet.scala
@@ -0,0 +1,16 @@
+package com.virtuslab.auctionHouse.sync
+
+class StatusServlet extends BaseServlet {
+
+  private lazy val version = System.getProperty("service.version", "unknown")
+
+  case class Status(version: String = version)
+
+  before() {
+    contentType = formats("json")
+  }
+
+  get("/_status") {
+    Status()
+  }
+}
