0f1ade66ea3aaf2c54390a594b3a2fed74c55749
mzagorski <zagorski.dev@gmail.com>
2018-04-13 12:49:34 +0200

    moving heavy requests to separate connections

--- codebase/billing-service-secondary-async/src/main/scala/com/virtuslab/billings/async/BillingRoutes.scala
+++ codebase/billing-service-secondary-async/src/main/scala/com/virtuslab/billings/async/BillingRoutes.scala
@@ -6,17 +6,19 @@ import akka.http.scaladsl.model.ContentTypes.`application/json`
 import akka.http.scaladsl.model.HttpMethods.POST
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.headers.RawHeader
-import akka.http.scaladsl.model.{HttpEntity, HttpRequest}
+import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse}
 import akka.http.scaladsl.server.Directives.{as, complete, entity, handleRejections, onComplete, optionalHeaderValueByName, path, post, _}
 import akka.http.scaladsl.server.Route
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.typesafe.scalalogging.Logger
 import com.virtuslab.base.async.{Http, IdentityHelpers, RoutesAuthSupport, RoutingUtils}
 import com.virtuslab.payments.payments.{Invoice, PaymentRequest}
 import com.virtuslab.{Config, TraceId, TraceIdSupport}
 import io.prometheus.client.Histogram
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+import akka.http.scaladsl.{Http => AkkaHttp}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.Future.failed
 import scala.util.{Failure, Success}
 
@@ -36,6 +38,14 @@ trait BillingRoutes extends SprayJsonSupport with DefaultJsonProtocol with Routi
 
   lazy val paymentSystemUrl = s"http://${Config.paymentSystemContactPoint}/api/v1/payment"
 
+  val connectionFlow: Flow[HttpRequest, HttpResponse, Future[AkkaHttp.OutgoingConnection]] =
+    AkkaHttp().outgoingConnection("payment-system", 9000)
+
+  def dispatchRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request)
+    .via(connectionFlow)
+    .runWith(Sink.head)
+
+
   lazy val billingRoutes: Route = handleRejections(rejectionHandler) {
     optionalHeaderValueByName("X-Trace-Id") { maybeTraceId =>
       implicit val traceId: TraceId = extractTraceId(maybeTraceId)
@@ -45,13 +55,14 @@ trait BillingRoutes extends SprayJsonSupport with DefaultJsonProtocol with Routi
             entity(as[PaymentRequest]) { request =>
               logger.info(s"[${traceId.id}] Received payment request '$request'.")
               val histogramTimer = requestsLatency.labels("payForAuction").startTimer()
-              val payRequest = HttpRequest(POST, paymentSystemUrl)
+              val payRequest = HttpRequest(POST, "/api/v1/payment")
                               .withHeaders(RawHeader("X-Trace-Id", traceId.id))
                               .withEntity(HttpEntity(`application/json`, payFormat.write(request).compactPrint))
-              onComplete(httpClient.flatMapRequest(payRequest) { httpResponse =>
-                if(httpResponse.status.isSuccess()) putInvoice(request) else failed(
-                  new RuntimeException("Unexpected response from payment system"))
-              }) {
+
+              onComplete((for {
+                _ <- callPaymentSystem(payRequest)
+                invoiceId <- putInvoice(request)
+              } yield invoiceId)) {
                 case Success(invoiceId) =>
                   logger.info(s"[${traceId.id}] payment completed successfully")
                   histogramTimer.observeDuration()
@@ -65,6 +76,20 @@ trait BillingRoutes extends SprayJsonSupport with DefaultJsonProtocol with Routi
           }
         }
       }
+    }
+  }
+
+  def callPaymentSystem(req: HttpRequest) = Future[Unit] {
+    dispatchRequest(req).flatMap { httpResponse =>
+      val res = if(httpResponse.status.isSuccess()) {
+        Future.successful(Unit)
+      } else {
+        failed(
+          new RuntimeException("Unexpected response from payment system"))
+      }
+      req.discardEntityBytes()
+      httpResponse.discardEntityBytes()
+      res
     }
   }
 }
