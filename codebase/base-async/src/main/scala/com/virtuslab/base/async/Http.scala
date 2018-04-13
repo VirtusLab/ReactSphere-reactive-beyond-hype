8e690b8156c1e82b32baa5c520d793aa136038c3
mzagorski <zagorski.dev@gmail.com>
2018-04-13 15:38:49 +0200

    global context removal

--- codebase/base-async/src/main/scala/com/virtuslab/base/async/Http.scala
+++ codebase/base-async/src/main/scala/com/virtuslab/base/async/Http.scala
@@ -5,15 +5,14 @@ import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.{HttpExt, Http => AHttp}
 import akka.stream.Materializer
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 
 object Http {
-  def apply()(implicit system: ActorSystem): Http = new Http(AHttp())
+  def apply()(implicit system: ActorSystem, executionContext: ExecutionContext): Http = new Http(AHttp())
 }
 
-class Http(private val akkaHttp: HttpExt) {
+class Http(private val akkaHttp: HttpExt)(implicit executionContext: ExecutionContext) {
   def mapRequest[T](httpRequest: HttpRequest)(logic: HttpResponse => T)
                    (implicit materializer: Materializer): Future[T] = {
     akkaHttp
